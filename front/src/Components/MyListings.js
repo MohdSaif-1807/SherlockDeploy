ca517080309d18e6b5aa6fca6265f9400e8163a5
MohdSaif-1807 <113005309+MohdSaif-1807@users.noreply.github.com>
2023-07-02 14:49:55 +0530

    Update MyListings.js

--- front/src/Components/MyListings.js
+++ front/src/Components/MyListings.js
@@ -30,7 +30,7 @@ export default function Feed() {
   useEffect(() => {
     // console.log("Test");
     Axios({
-      url: `http://localhost:5000/mylistings/${
+      url: `https://sherlock-backend-final.onrender.com/mylistings/${
         JSON.parse(localStorage.getItem("user"))._id
       }`,
       method: "GET",
