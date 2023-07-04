35cef6c8ac7f62880f4398c85e0060fc962c4880
MohdSaif-1807 <113005309+MohdSaif-1807@users.noreply.github.com>
2023-07-04 19:43:18 +0530

    Update Login.js

--- front/src/Components/Login.js
+++ front/src/Components/Login.js
@@ -19,7 +19,7 @@ function Login() {
       password: document.getElementById("password").value,
     };
     axios({
-      url: "https://sherlock-backend-end.onrender.com/login",
+      url: "https://sherlock-back.onrender.com/login",
       method: "POST",
       data: payload,
 
